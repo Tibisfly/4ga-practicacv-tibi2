a96b55ce675ceaefa51e7680f2a96ffafe6d1e78
Tibisfly <vielmatorotibisay@gmail.com>
2020-10-08 10:34:32 +0000

    resumeTibi

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ require('./style.css')
 /**
  * Estructure sus datos de la forma que considere
  */
-const data = {
+// const data = {
 
-}
+// }
 
-window.onload = function() {
-  /* Modifique el código en este bloque */
+// window.onload = function() {
+//   /* Modifique el código en este bloque */
 
-  document.body.innerHTML = "<h1>My Resumee</h1>"
+//   document.body.innerHTML = "<h1>My Resumee</h1>"
 
-  /* Fin del bloque*/
-}
+//   /* Fin del bloque*/
+// }
